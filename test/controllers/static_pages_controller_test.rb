2f02ead2f33425104600f6e27e5d70b0575d57ee
ymanam328 <ymnn28@gmail.com>
2019-11-12 20:18:15 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  #Homeのテスト
+  #GETリクエストをhomeアクションに対して送信.リクエストに対するレスポンスは成功になる
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -11,4 +23,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
